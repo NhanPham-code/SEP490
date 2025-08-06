fc80c665a40debd71028f858bf8fdc96f992b000
Thuc tap Sinh 1 <thuctapsinh1@mobifone.vn>
2025-08-06 20:24:57 +0700

    add TimeZone View

---
+++ Client/CustomerUI/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerUI.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult TimeZone()
+        {
+            return View();
+        }
+    }
+}
