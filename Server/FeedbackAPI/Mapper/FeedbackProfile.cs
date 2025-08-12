859259b66b3abfd175937d9f8e5a83f1ddb35a5b
unknown <dangvohong2k4@gmail.com>
2025-08-12 16:17:14 +0700

    map

---
+++ Server/FeedbackAPI/Mapper/FeedbackProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FeedbackAPI.DTOs;
+using FeedbackAPI.Models;
+
+namespace FeedbackAPI.Mapper
+{
+    public class FeedbackProfile : Profile
+    {
+        public FeedbackProfile()
+        {
+            CreateMap<CreateFeedback, Feedback>();
+            CreateMap<UpdateFeedback, Feedback>();
+            CreateMap<Feedback, FeedbackResponse>();
+        }
+    }
+}
---
+++ Server/FeedbackAPI/Service/IFeedbackService.cs
@@ -0,0 +1,15 @@
+﻿using FeedbackAPI.DTOs;
+
+namespace FeedbackAPI.Service
+{
+    public interface IFeedbackService
+    {
+        Task<IEnumerable<FeedbackResponse>> GetAllAsync();
+        Task<FeedbackResponse?> GetByIdAsync(int id);
+        Task<FeedbackResponse> CreateAsync(CreateFeedback dto);
+        Task<bool> UpdateAsync(int id, UpdateFeedback dto);
+        Task<bool> DeleteAsync(int id);
+
+        IQueryable<FeedbackResponse> GetAllAsQueryable();
+    }
+}
