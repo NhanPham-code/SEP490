b5559e86a1f94ff94597e2303bdd12319195fb66
Thuc tap Sinh 1 <thuctapsinh1@mobifone.vn>
2025-08-20 16:39:25 +0700

    add manager

---
+++ Client/StadiumManagerUI/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StadiumManagerUI.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult BookingManager()
+        {
+            return View();
+        }
+    }
+}
