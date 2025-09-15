effd5619dfa693f07edd0b843984fea04c28ed09
DatLNT <datle2212@gmail.com>
2025-09-15 12:32:00 +0700

    save Find Team

---
+++ Client/DTOs/FindTeamDTO/BookingAndStadiumViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DTOs.BookingDTO;
+using DTOs.StadiumDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.FindTeamDTO
+{
+    public class BookingAndStadiumViewModel
+    {
+        public List<BookingReadDto> Bookings { get; set; }
+        public Dictionary<int, ReadStadiumDTO> Stadiums { get; set; } = new Dictionary<int, ReadStadiumDTO>();
+
+    }
+}
